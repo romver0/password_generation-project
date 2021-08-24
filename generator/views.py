ffb28c773fa5f58e91df13fab243ae2af70a5a30
Roman <roma_zverev2002@mail.ru>
2021-08-24 07:31:02 +0300

    Our first commit

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
@@ -8,5 +9,22 @@ def home(request):
     return render(request, 'generator/home.html')
 
 
+def description(request):
+    return render(request, 'generator/description.html')
+
+
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+-/"№;%:?'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length ', 12))
+    the_password = ''
+    for x in range(length):
+        the_password += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': the_password})
--- password_generation/urls.py
+++ password_generation/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
-    path('password/', views.password,name='password'),
+    path('', views.home,name="home"),
+    path('password/', views.password, name='password'),
+    path('description/', views.description, name='description'),
 ]
